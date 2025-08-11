e447e5bd3e307b2082aafb4a468c267302fd8450
Ptl-sneh <snehpatel641@gmail.com>
2025-08-11 23:41:12 +0530

    s

--- start.sh
+++ start.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+# Create /data folder if it doesn't exist
+mkdir -p /data
+
+# Run Django migrations (will create the SQLite DB in /data/db.sqlite3)
 python disaster_backend/manage.py migrate
+
+# Collect static files
 python disaster_backend/manage.py collectstatic --noinput
+
+# Start Django server
 python disaster_backend/manage.py runserver 0.0.0.0:10000
